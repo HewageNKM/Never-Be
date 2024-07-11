333753875e47e49741b168aba41e0022c2eef5fc
Nadun Kawishika <107237482+HewageNKM@users.noreply.github.com>
2024-07-11 12:12:23 +0530

    Basic Structure complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Header from "@/components/sections/Header";
+import ImageSlider from "@/components/sections/ImageSlider";
+import Popular from "@/components/sections/Popular";
+import Arrival from "@/components/sections/Arrival";
+import Footer from "@/components/sections/Footer";
+
 export default function Home() {
     return (
-        <main className="w-full h-full">
-            Never Be
+        <main className="w-full overflow-clip h-full px-8 py-4">
+            <Header/>
+            <ImageSlider />
+            <Popular />
+            <Arrival />
+            <Footer/>
         </main>
     );
 }
