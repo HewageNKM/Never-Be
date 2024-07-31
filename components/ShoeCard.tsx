75ca1bf46c4d47855ca3348001c131e3ff6189e8
Nadun Kawshika <107237482+HewageNKM@users.noreply.github.com>
2024-07-31 11:54:34 +0530

    OutOfStocks Label added to ShoeCard.tsx

--- components/ShoeCard.tsx
+++ components/ShoeCard.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import {Button, Rating} from "@mui/material";
@@ -8,6 +8,7 @@ const ShoeCard = ({shoe, type}: {
     shoe: Shoe,
     type?: string
 }) => {
+    const [outOfStock, setOutOfStock] = useState(false);
     const [addToCart, setAddToCart] = useState(false);
     const [isMouseOver, setIsMouseOver] = useState(false);
     const [selectedSize, setSelectedSize] = useState<number>(0);
@@ -15,8 +16,29 @@ const ShoeCard = ({shoe, type}: {
 
     const setAvailableSizes = (size: number, index: number) => {
         const stocks = shoe.stocks;
-        return <motion.button initial={{opacity:0,y:"1vh"}} animate={{opacity:`${stocks[size] <= 0 ? '0.5':'1'}`,y:0}} transition={{duration:0.5}} exit={{opacity:0,y:'1vh'}} key={index} onClick={() => {setSelectedSize(size)}} className={`capitalize p-2 rounded-md ${selectedSize === size ? 'bg-primary text-white' : 'bg-gray-200 text-black'}}`} disabled={stocks[size] <= 0}>{size}</motion.button>
+        return <motion.button initial={{opacity: 0, y: "1vh"}}
+                              animate={{opacity: `${stocks[size] <= 0 ? '0.5' : '1'}`, y: 0}}
+                              transition={{duration: 0.5}} exit={{opacity: 0, y: '1vh'}} key={index} onClick={() => {
+            setSelectedSize(size)
+        }}
+                              className={`capitalize p-2 rounded-md ${selectedSize === size ? 'bg-primary text-white' : 'bg-gray-200 text-black'}}`}
+                              disabled={stocks[size] <= 0}>{size}</motion.button>
     }
+    const checkStocksAvailability = () => {
+        const stocks = shoe.stocks;
+        let count = 0;
+        for (let i = 0; i < stocks.length; i++) {
+            if (stocks[i] <= 0) {
+                count++;
+            }
+        }
+        if (count === stocks.length) {
+            setOutOfStock(true)
+        }
+    }
+    useEffect(() => {
+        checkStocksAvailability()
+    })
     return (
         <div onMouseEnter={() => setIsMouseOver(true)}
              onMouseLeave={() => {
@@ -44,7 +66,7 @@ const ShoeCard = ({shoe, type}: {
                         <div className="relative w-fit">
                             <div className="h-[1.1px] absolute top-1/2 left-0 w-[4.2rem] bg-black"/>
                             <div className="flex flex-row items-center justify-center gap-3">
-                                <h2 className="font-medium text-lg">රු {((shoe.discount+100)*shoe.sellingPrice/100)}</h2>
+                                <h2 className="font-medium text-lg">රු {((shoe.discount + 100) * shoe.sellingPrice / 100)}</h2>
                                 <p className="bg-red-600 text-sm px-1 rounded text-white">-{shoe.discount}</p>
                             </div>
                         </div>
@@ -56,13 +78,17 @@ const ShoeCard = ({shoe, type}: {
             </Link>
             <AnimatePresence>
                 {addToCart && (
-                    <motion.div initial={{y:"1vh",opacity:0}} animate={{y:0,opacity:1}} exit={{y:"1vh",opacity:0}} className="flex flex-col mt-2 justify-center items-center">
+                    <motion.div initial={{y: "1vh", opacity: 0}} animate={{y: 0, opacity: 1}}
+                                exit={{y: "1vh", opacity: 0}}
+                                className="flex flex-col mt-2 justify-center items-center">
                         <div className="flex flex-row flex-wrap gap-1 text-sm">
                             {shoe.sizes?.map((size: number, index: number) => (
                                 setAvailableSizes(size, index)
                             ))}
                         </div>
-                        <motion.div initial={{opacity:0,y:"1vh"}} animate={{opacity:1,y:0}} transition={{duration:0.5}} exit={{opacity:0, y:'1vh'}} className='w-full mt-2 justify-between items-center gap-2 flex-row flex'>
+                        <motion.div initial={{opacity: 0, y: "1vh"}} animate={{opacity: 1, y: 0}}
+                                    transition={{duration: 0.5}} exit={{opacity: 0, y: '1vh'}}
+                                    className='w-full mt-2 justify-between items-center gap-2 flex-row flex'>
                             <Button onClick={() => {
                                 setAddToCart(false)
                                 setSelectedSize(0)
@@ -75,17 +101,18 @@ const ShoeCard = ({shoe, type}: {
                     </motion.div>
                 )}
                 {(isMouseOver && !addToCart) && (
-                    <motion.div className="lg:block hidden" initial={{opacity: 0, y: "2vh"}} animate={{opacity: 1, y: 0}}>
-                        <Button onClick={() => setAddToCart(true)}
-                                className="bg-primary hover:bg-primary-100 text-white mt-2 p-1 rounded-md w-full text-center">Add
+                    <motion.div className="lg:block hidden" initial={{opacity: 0, y: "2vh"}}
+                                animate={{opacity: 1, y: 0}}>
+                        <Button disabled={outOfStock} onClick={() => setAddToCart(true)}
+                                className={`bg-primary hover:bg-primary-100 text-white mt-2 p-1 rounded-md w-full text-center ${outOfStock && "opacity-50"}`}>Add
                             to
                             Cart</Button>
 
                     </motion.div>
                 )}
             </AnimatePresence>
-            {!addToCart && (<Button onClick={() => setAddToCart(true)}
-                                   className="bg-primary lg:hidden block hover:bg-primary-100 text-white mt-2 p-1 rounded-md w-full text-center">Add
+            {!addToCart && (<Button disabled={outOfStock} onClick={() => setAddToCart(true)}
+                                    className="bg-primary lg:hidden block hover:bg-primary-100 text-white mt-2 p-1 rounded-md w-full text-center">Add
                 to
                 Cart</Button>)}
             {type === "new" &&
@@ -93,6 +120,12 @@ const ShoeCard = ({shoe, type}: {
                     className="bg-green-400 font-light absolute text-white top-1 left-1 text-sm w-fit p-1">New</div>}
             {type === "popular" &&
                 <div className="bg-red-500 font-light text-white absolute top-1 left-1 text-sm w-fit p-1">Hot</div>}
+            {outOfStock && (<div
+                className="bg-white rounded-lg shadow-custom font-semibold text-black absolute top-1 right-1 text-sm w-fit p-1">Out
+                of
+                Stock
+            </div>)
+            }
         </div>
     );
 };
